89254276be967853ee4386c06dbcfb8792478fd4
keksite <keksite@mail.ru>
2020-08-18 18:36:27 +0300

    added YandexTest

---
+++ src/test/java/YandexTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTest {
+    @Test
+    void selenideSearchTest() {
+
+        Configuration.browser = "org.openqa.selenium.safari.SafariDriver";
+        // Открыть google
+        open("https://yandex.ru");
+
+        // Ввести Selenide в поиск
+        $(".input__control.input__input").setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+}
